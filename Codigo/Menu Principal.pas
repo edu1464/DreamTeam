0fcc98645cc16f911fe0aa7e54311d9ff0cc4376
PabloSabba <pablo.sabba55@gmail.com>
2016-06-19 22:28:35 -0300

    Arreglado

--- Codigo/Menu Principal.pas
+++ Codigo/Menu Principal.pas
@@ -48,81 +48,82 @@ begin
      tiempo:= 5000;
      While fin Do
      Begin
+          DibujarPal('Nro ' + intToStr(cont),1,1,0,'AsciiArtFont03');
+          DibujarPal('Visualice el numero',1,12,0,'AsciiArtFont03');
+          TemporVisual:='##################################################';
+          GotoXY (22,23);
+          Write ('          __________________________________________________');
+          GotoXY (22,24);
+          Write ('         /                                                  \');
+          GotoXY (22,25);
+          Write ('TIEMPO: ( ',TemporVisual);
+          GotoXY (22,26);
+          Write ('         \__________________________________________________/');
 
-     DibujarPal('Nro ' + intToStr(cont),1,1,0,'AsciiArtFont03');
-     DibujarPal('Visualice el numero',1,12,0,'AsciiArtFont03');
-     TemporVisual:='##################################################';
-     GotoXY (22,23);
-     Write ('          __________________________________________________');
-     GotoXY (22,24);
-     Write ('         /                                                  \');
-     GotoXY (22,25);
-     Write ('TIEMPO: ( ',TemporVisual);
-     GotoXY (22,26);
-     Write ('         \__________________________________________________/');
 
+          aleat:= GenNum();
+          Dibujando(aleat, 0, 30, 30);
 
-     aleat:= GenNum();
-     Dibujando(aleat, 0, 30, 30);
-
-     {Desorden del numero}
-     Randomize;
-     desorden:= '';
-     resgPos:='000000';
-     flag1:= True;
-     j:= 1;
-     While flag1 Do
-     Begin
-          flag2:= True;
-          pos:= Random(7);
-          If pos > 0 Then
+          {Desorden del numero}
+          Randomize;
+          desorden:= '';
+          resgPos:='000000';
+          flag1:= True;
+          j:= 1;
+          While flag1 Do
           Begin
-               For i:= 1 to 6 Do
+               flag2:= True;
+               pos:= Random(7);
+               If pos > 0 Then
                Begin
-                    If (resgPos[i] = intToStr(pos)[1]) and (flag2) Then
-                       flag2:= False;
-               End;
+                    For i:= 1 to 6 Do
+                    Begin
+                         If (resgPos[i] = intToStr(pos)[1]) and (flag2) Then
+                         flag2:= False;
+                    End;
 
-               If flag2 Then
-               Begin
-                    resgPos[j]:=(intToStr(pos)[1]);
-                    desorden:= desorden + aleat[pos];
-                    j:= j+1;
-                    If (j > 6) and (desorden <> aleat) Then
+                    If flag2 Then
                     Begin
-                         flag1:= False;
-                    End
-                    Else
-                    Begin
-                         If (j > 6) and (desorden = aleat) Then
-                            j:= 1;
+                         resgPos[j]:=(intToStr(pos)[1]);
+                         desorden:= desorden + aleat[pos];
+                         j:= j+1;
+                         If (j > 6) and (desorden <> aleat) Then
+                         Begin
+                              flag1:= False;
+                         End
+                         Else
+                         Begin
+                              If (j > 6) and (desorden = aleat) Then
+                              j:= 1;
+                         End;
                     End;
                End;
           End;
-     End;
   
-      {Espera de 5 seg para memorizar el numero}
+          {Espera de 5 seg para memorizar el numero}
 
-     Repeat
-     Begin
-           Delay (tiempo DIV 50);
-           GotoXY (22,25);
-           Delete (TemporVisual, length(TemporVisual),1);
-           clrEol;
-           Write ('TIEMPO: ( ',TemporVisual);
-     end
-     until length(TemporVisual) = 0;
+          Repeat
+          Begin
+               Delay (tiempo DIV 50);
+               GotoXY (22,25);
+               Delete (TemporVisual, length(TemporVisual),1);
+               clrEol;
+               Write ('TIEMPO: ( ',TemporVisual);
+          end
+          until length(TemporVisual) = 0;
 
-     salir:= True;
-     vida:= 3;
-     While salir Do
-     Begin
-          clrscr;
-          GotoXY (10,12);
-          Write ('Intentos:');
-          For i:=1 to vida do
+          salir:= True;
+          vida:= 3;
+          While salir Do
+          Begin
+
+               If vida >= 1 Then
+               Begin
+                    clrscr;
+                    GotoXY (10,12);
+                    Write ('Intentos:');
+                    For i:=1 to vida do
                     Begin
-                         GotoXY (22+ (15*i) ,10);
                          Write ('  oo   oo  ');
                          GotoXY (22+ (15*i) ,11);
                          Write ('ooooooooooo');
@@ -133,65 +134,62 @@ begin
                          GotoXY (22+ (15*i) ,14);
                          Write ('     o  ');
                     end;
-          DibujarPal('Adivina',33,17,1,'AsciiArtFont03');
-          GotoXY (10,33);
-          Write ('Numero desordenado:');
-          Dibujando(desorden, 0, 30, 30);
-          GotoXY (10,40);
-          Write ('Ingresa el numero original:');
-          GoToXY(40, 40);
-          Readln(numIngresado);
-          If vida > 1 Then
-          Begin
-               If numIngresado = aleat Then
-               Begin
-                    salir:= False;
-                    cont:= cont + 1;
-                    GotoXY (55,50);
-                    Write ('Acertaste!');
-                    Case vida of
-                         3: puntaje:= puntaje + 100;
-                         2: puntaje:= puntaje + 50;
-                         1: puntaje:= puntaje + 25;
+                    DibujarPal('Adivina',33,17,1,'AsciiArtFont03');
+                    GotoXY (10,33);
+                    Write ('Numero desordenado:');
+                    Dibujando(desorden, 0, 30, 30);
+                    GotoXY (10,40);
+                    Write ('Ingresa el numero original:');
+                    GoToXY(40, 40);
+                    Readln(numIngresado);
+                    If numIngresado = aleat Then
+                    Begin
+                         salir:= False;
+                         cont:= cont + 1;
+                         GotoXY (55,50);
+                         Write ('Acertaste!');
+                         Case vida of
+                              3: puntaje:= puntaje + 100;
+                              2: puntaje:= puntaje + 50;
+                              1: puntaje:= puntaje + 25;
+                         End;
+                    End
+                    Else
+                    Begin
+                         ClrScr;
+                         vida:= vida - 1;
+                         DibujarPal('Te queda ' + intToStr(vida) + ' vidas',1,20,0,'AsciiArtFont01');
+                         For i:=1 to vida do
+                         Begin
+                              GotoXY (30+ (15*i) ,30);
+                              Write ('  oo   oo  ');
+                              GotoXY (30+ (15*i) ,31);
+                              Write ('ooooooooooo');
+                              GotoXY (30+ (15*i) ,32);
+                              Write (' ooooooooo ');
+                              GotoXY (30+ (15*i) ,33);
+                              Write ('   ooooo  ');
+                              GotoXY (30+ (15*i) ,34);
+                              Write ('     o  ');
+                         end;
+
+                         Delay(2000);
                     End;
                End
                Else
                Begin
                     ClrScr;
-                    vida:= vida - 1;
-                    DibujarPal('Te queda ' + intToStr(vida) + ' vidas',1,20,0,'AsciiArtFont01');
-                    For i:=1 to vida do
-                    Begin
-                         GotoXY (30+ (15*i) ,30);
-                         Write ('  oo   oo  ');
-                         GotoXY (30+ (15*i) ,31);
-                         Write ('ooooooooooo');
-                         GotoXY (30+ (15*i) ,32);
-                         Write (' ooooooooo ');
-                         GotoXY (30+ (15*i) ,33);
-                         Write ('   ooooo  ');
-                         GotoXY (30+ (15*i) ,34);
-                         Write ('     o  ');
-                    end;
-
-                    Delay(2000);
+                    Dibujando('PERDISTE', 20, 20, 20);
+                    salir:= False;
+                    fin:= False;
                End;
-          End
-          Else
-          Begin
-               ClrScr;
-               Dibujando('PERDISTE', 20, 20, 20);
-               salir:= False;
-               fin:= False;
+               if (cont mod 3) = 0 Then
+               Begin
+                    tiempo:= tiempo - 500;
+               End;
           End;
-          if (cont mod 3) = 0 Then
-             Begin
-                  tiempo:= tiempo - 500;
-          End;
-     End;
-     delay (1500);
-     ClrScr;
-
+          delay (1500);
+          ClrScr;
      End;
      GotoXY (20,5);
      Writeln('Ingrese nombre');
@@ -233,14 +231,7 @@ begin
      end
      else
      DibujarPal(intToStr(puntaje),22,37,0,'AsciiArtFont04');
-
-
-
-
      GuardarPuntaje(puntaje,nombre);
-
-
-
      ReadKey;
      control:= false;
      ClrScr;
